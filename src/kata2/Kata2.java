494b466243ee19c740aff67e776ec2e7f9c113d9
Isac <isac.as0001@gmail.com>
2021-10-03 19:37:31 +0100

    Kata2 versión 5

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -6,14 +6,9 @@ public class Kata2 {
 
     public static void main(String[] args) {
 
-        Integer[] data = new Integer[15];
-        Random rand = new Random();
-        for (int i = 0; i < data.length; i++) {
-            data[i] = rand.nextInt(15);
-        }
-        
+        String[] data = {"Rosa", "Pepe", "María", "Pepe", "Pepe", "Rosa"};
         Histogram histogram = new Histogram(data);
-        Map<Integer, Integer> histogr = histogram.getHistogram();
+        Map<String, Integer> histogr = histogram.getHistogram();
         histogr.keySet().forEach((key) -> {
             System.out.println(key + "==>" + histogr.get(key));
         });
