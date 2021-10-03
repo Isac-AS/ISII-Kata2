b3774ca3a06a45bb401f6e523642aad60595d84f
Isac <isac.as0001@gmail.com>
2021-10-03 18:53:16 +0100

    Kata2 versión 2

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -13,14 +13,10 @@ public class Kata2 {
         }
         HashMap<Integer, Integer> histogram = new HashMap<>();
 
-        for (int i = 0; i < data.length; i++) {
-            if (histogram.containsKey(data[i])) {
-                histogram.put(data[i], histogram.get(data[i]) + 1);
-            } else {
-                histogram.put(data[i], 1);
-            }
+        for (int key : data) {
+            histogram.put(key, histogram.containsKey(key) ? histogram.get(key) + 1 : 1);
         }
-
+        
         histogram.keySet().forEach((key) -> {
             System.out.println(key + "==>" + histogram.get(key));
         });
